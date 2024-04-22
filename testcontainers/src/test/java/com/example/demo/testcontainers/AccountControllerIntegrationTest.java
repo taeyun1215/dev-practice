657ff130d3dbb5c85c227b7e0ed7eb68081236cc
taeyun1215 <taeyun1215@naver.com>
2024-04-22 22:57:57 +0900

    feat : test container 테스트 코드 작성

--- testcontainers/src/test/java/com/example/demo/testcontainers/AccountControllerIntegrationTest.java
+++ testcontainers/src/test/java/com/example/demo/testcontainers/AccountControllerIntegrationTest.java
@@ -1,33 +1,45 @@
-package com.example.demo;
+package com.example.demo.testcontainers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")  // Use application-test.yml configuration
+@Testcontainers
 public class AccountControllerIntegrationTest {
+
+    @Container
+    public static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:10.5")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(true);
+
+    @DynamicPropertySource
+    static void setDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mariaDBContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mariaDBContainer::getUsername);
+        registry.add("spring.datasource.password", mariaDBContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.mariadb.jdbc.Driver");
+    }
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
     @Test
     public void testAccountCreationAndRetrieval() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
         String accountJson = "{\"owner\":\"John Doe\",\"balance\":1000.0}";
 
         mockMvc.perform(post("/api/accounts")
