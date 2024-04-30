15ff728d072bf02d71b7fd847d28ec666a7dbd9a
taeyun1215 <taeyun1215@naver.com>
2024-04-30 22:29:08 +0900

    feat : account api 추가

---
+++ testcontainers/src/main/java/com/example/demo/controller/payload/AccountDto.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountDto {
+
+    private String owner;
+    private double balance;
+
+}
