dbf8f18859592c03b0ec2edee3c4fdef7cf06c3b
taeyun1215 <taeyun1215@naver.com>
2024-05-02 22:41:34 +0900

    feat : AccountDto에 @AllArgsConstructor 추가

--- testcontainers/src/main/java/com/example/demo/controller/payload/AccountDto.java
+++ testcontainers/src/main/java/com/example/demo/controller/payload/AccountDto.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class AccountDto {
 
     private String owner;
