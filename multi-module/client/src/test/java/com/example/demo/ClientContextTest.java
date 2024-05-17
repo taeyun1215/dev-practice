3165603a5690578bcafffb90c11f92453e8378fe
taeyun1215 <taeyun1215@naver.com>
2024-05-17 15:06:49 +0900

    feat : client module TestPropertySource 추가

--- multi-module/client/src/test/java/com/example/demo/ClientContextTest.java
+++ multi-module/client/src/test/java/com/example/demo/ClientContextTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestPropertySource;
 
 @Tag("context")
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("test") // application-test.yml일 경우엔 이렇게 사용
+@TestPropertySource(properties = {"spring.config.location = classpath:client-application-test.yml"})
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public abstract class ClientContextTest {
 
