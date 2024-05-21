3ca1709d404b5b9e62a17fe7b339eb090cf28fb1
taeyun1215 <taeyun1215@naver.com>
2024-05-21 15:45:07 +0900

    feat : db module 테스트 코드 수정

--- multi-module/storage/rdbms/src/test/java/com/example/demo/RdbmsContextTest.java
+++ multi-module/storage/rdbms/src/test/java/com/example/demo/RdbmsContextTest.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MariaDBContainer;
@@ -22,6 +21,7 @@ public abstract class RdbmsContextTest {
 	// MariaDB 컨테이너 설정
 	@Container
 	static MariaDBContainer<?> MARIADB_CONTAINER = new MariaDBContainer<>("mariadb:10.11")
-		.withInitScript("data/init.sql");
+		;
+		// .withInitScript("data/init.sql");
 
 }
