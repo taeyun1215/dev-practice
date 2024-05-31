26d8f5e1d8779e9f7832739bf5791ec4321568ee
taeyun1215 <taeyun1215@naver.com>
2024-05-31 16:05:06 +0900

    fix : test code display name 변경

--- multi-module/client/src/test/java/com/example/demo/NotificationServiceClientTest.java
+++ multi-module/client/src/test/java/com/example/demo/NotificationServiceClientTest.java
@@ -1,18 +1,20 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class NotificationServiceClientTest extends ClientContextTest {
+class NotificationServiceClientTest extends ClientContextTest {
 
     @Autowired
     private NotificationServiceClient notificationServiceClient;
 
     @Test
-    public void testSendNotification() {
+    @DisplayName("외부 시스템 sendNotification 통합 테스트")
+    void sendNotificationTest() {
         NotificationRequest request = new NotificationRequest("user@example.com", "Your order has been shipped!");
         NotificationResponse response = notificationServiceClient.sendNotification(request);
         assertNotNull(response);
