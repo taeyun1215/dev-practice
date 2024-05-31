0541f41b1d933bd1c92c89b825f2e261bcf2c98c
taeyun1215 <taeyun1215@naver.com>
2024-05-31 14:56:09 +0900

    :recycle: application-test.yml 사용 안 함

--- multi-module/client/src/test/java/com/example/demo/ClientContextTest.java
+++ multi-module/client/src/test/java/com/example/demo/ClientContextTest.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestPropertySource;
 
 @Tag("context")
 @SpringBootTest
-@ActiveProfiles("test") // application-test.yml일 경우엔 이렇게 사용
+//@ActiveProfiles("test") // application-test.yml일 경우엔 이렇게 사용
 @TestPropertySource(properties = {"spring.config.location = classpath:client-application-test.yml"})
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public abstract class ClientContextTest {
