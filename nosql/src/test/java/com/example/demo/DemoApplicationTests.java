9339f0c9c33b6631cddfb6990150f0bb98e2ed33
taeyun1215 <taeyun1215@naver.com>
2024-06-03 17:05:45 +0900

    feat : nosql module init

---
+++ nosql/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,9 @@
+package nosql.src.test.java.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+}
